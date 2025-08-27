8b45fef3dfb71b798fe3eb2a343584ca5565dba2
SolovyevEgor <egor_spaik05@mail.ru>
2025-08-27 12:31:23 +0300

    test(orm): simple insert smoke using Db.exec and Stmt

---
+++ tests-cpp/orm_smoke.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "../src-cpp/mplx-orm/db.hpp"
+#include <filesystem>
+
+TEST(Orm, Smoke){
+  namespace fs = std::filesystem;
+  auto path = fs::temp_directory_path() / "mplx_test.sqlite";
+  if (fs::exists(path)) fs::remove(path);
+  mplx::orm::Db db(path.string());
+  db.exec("CREATE TABLE t(a INTEGER);");
+  mplx::orm::Stmt ins(db.handle(), "INSERT INTO t(a) VALUES(1);");
+  ins.step();
+  // no crash is ok for smoke
+  SUCCEED();
+}
+
+
