f6eed828b16ffa7f83677250d0b9bb5a561ba85c
SolovyevEgor <egor_spaik05@mail.ru>
2025-08-27 11:46:17 +0300

    feat(lsp): minimal TS language server skeleton

---
+++ src/Mplx.Lsp/src/server.ts
@@ -0,0 +1,19 @@
+﻿import { createConnection, ProposedFeatures, InitializeParams, InitializeResult, TextDocuments, TextDocument } from 'vscode-languageserver'
+
+const connection = createConnection(ProposedFeatures.all)
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
+
+connection.onInitialize((_params: InitializeParams): InitializeResult => {
+  return {
+    capabilities: {
+      textDocumentSync: 1,
+    }
+  }
+})
+
+documents.onDidOpen(e => {
+  connection.console.log(`Opened ${e.document.uri}`)
+})
+
+documents.listen(connection)
+connection.listen()
